42c4e5b6a1aee2b315214603a6bfa35dfe91d05c
Herman Zuiakov <gzuyakov@gmail.com>
2022-12-05 15:22:05 +0100

    Set up express server and mongodb database

--- index.js
+++ index.js
@@ -3,8 +3,10 @@ const express = require('express'),
     mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/local',{useMongoClient: true}, ()=>{
-    console.log('CONNECTED TO DBS')
+    console.log('CONNECTED TO DBS');
 })
+
+
 
 app.listen(4111, ()=>{
     console.log('LISTENING ON PORT');
